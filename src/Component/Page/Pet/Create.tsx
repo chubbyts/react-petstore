4ebc113ba2f299231430b62ba0f7ca52097ca246
Dominik Zogg <dominik.zogg@gmail.com>
2020-03-01 12:52:58 +0100

    better 500 handling

--- src/Component/Page/Pet/Create.tsx
+++ src/Component/Page/Pet/Create.tsx
@@ -4,36 +4,45 @@ import { CreatePet } from '../../../ApiClient/Pet';
 import { Link, useHistory } from 'react-router-dom';
 import HttpError from '../../../Type/Error/HttpError';
 import InternalServerError from '../../../Type/Error/InternalServerError';
-import PageInternalServerError from '../Error/InternalServerError';
 import Pet from '../../../Type/Pet/Pet';
+import { Message } from 'semantic-ui-react';
 import PetForm from '../../Form/PetForm';
+import UnprocessableEntity from '../../../Type/Error/UnprocessableEntity';
 
 const Create = () => {
 
     const history = useHistory();
 
-    const [pet, setPet] = useState<Pet | InternalServerError>();
+    const [pet, setPet] = useState<Pet | UnprocessableEntity>();
+    const [internalServerError, setInternalServerError] = useState<InternalServerError>();
 
     document.title = 'Create Pet';
 
     const submitPet = async (pet: Pet) => {
         const responsePet = await CreatePet(pet);
 
-        setPet(responsePet);
+        if (responsePet instanceof InternalServerError) {
+            setInternalServerError(responsePet);
+        } else {
+            setInternalServerError(undefined);
+            setPet(responsePet);
+        }
 
         if (!(responsePet instanceof HttpError)) {
             history.push('/pet');
         }
     };
 
-    if (pet instanceof InternalServerError) {
-        return (
-            <PageInternalServerError />
-        );
-    }
-
     return (
         <main className='ui padded grid'>
+            {internalServerError ? (
+                <div className='row'>
+                    <Message negative className='attached segment'>
+                        <Message.Header>{internalServerError.title}</Message.Header>
+                        <p>{internalServerError.detail}</p>
+                    </Message>
+                </div>
+            ) : ''}
             <div className='row'>
                 <h1 className='ui huge dividing header'>Create Pet</h1>
             </div>
--- src/Component/Page/Pet/Update.tsx
+++ src/Component/Page/Pet/Update.tsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Message } from 'semantic-ui-react';
 import { Link, RouteComponentProps, useHistory } from 'react-router-dom';
 import { ReadPet, UpdatePet } from '../../../ApiClient/Pet';
 import Empty from '../Empty';
 import InternalServerError from '../../../Type/Error/InternalServerError';
 import NotFound from '../../../Type/Error/NotFound';
-import PageInternalServerError from '../Error/InternalServerError';
 import PageNotFound from '../Error/NotFound';
 import Pet from '../../../Type/Pet/Pet';
 import PetForm from '../../Form/PetForm';
@@ -18,7 +17,8 @@ const Update = ({ match }: RouteComponentProps<{ id: string; }>) => {
 
     const history = useHistory();
 
-    const [pet, setPet] = useState<Pet | InternalServerError | NotFound | UnprocessableEntity>();
+    const [pet, setPet] = useState<Pet | NotFound | UnprocessableEntity>();
+    const [internalServerError, setInternalServerError] = useState<InternalServerError>();
 
     useEffect(() => {
         const fetchPet = async () => {
@@ -33,7 +33,12 @@ const Update = ({ match }: RouteComponentProps<{ id: string; }>) => {
     const submitPet = async (pet: Pet) => {
         const responsePet = await UpdatePet(pet);
 
-        setPet(responsePet);
+        if (responsePet instanceof InternalServerError) {
+            setInternalServerError(responsePet);
+        } else {
+            setInternalServerError(undefined);
+            setPet(responsePet);
+        }
 
         if (!(responsePet instanceof HttpError)) {
             history.push('/pet');
@@ -44,16 +49,20 @@ const Update = ({ match }: RouteComponentProps<{ id: string; }>) => {
         return (<Empty />);
     }
 
-    if (pet instanceof InternalServerError) {
-        return (<PageInternalServerError />);
-    }
-
     if (pet instanceof NotFound) {
         return (<PageNotFound message={`Missing pet: ${id}`} />);
     }
 
     return (
         <main className='ui padded grid'>
+            {internalServerError ? (
+                <div className='row'>
+                    <Message negative className='attached segment'>
+                        <Message.Header>{internalServerError.title}</Message.Header>
+                        <p>{internalServerError.detail}</p>
+                    </Message>
+                </div>
+            ) : ''}
             <div className='row'>
                 <h1 className='ui huge dividing header'>Update Pet</h1>
             </div>
