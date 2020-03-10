eb702898198a6a4e93103c28349b07b2dd1f850a
Dominik Zogg <dominik.zogg@gmail.com>
2020-03-10 20:41:34 +0100

    add InvalidParameterByNameDenormalizer test

---
+++ src/Denormalizer/InvalidParameterByNameDenormalizer.test.ts
@@ -0,0 +1,22 @@
+import InvalidParameterByNameDenormalizer from './InvalidParameterByNameDenormalizer';
+
+test('denormalize', () => {
+    const invalidParameters = [
+        {name: 'name', reason: 'wrong type', details: {key: 'value1'}},
+        {name: 'name', reason: 'not empty', details: {key: 'value2'}},
+        {name: 'description', reason: 'to long', details: {key: 'value3'}}
+    ];
+
+    const invalidParameterByName = InvalidParameterByNameDenormalizer(invalidParameters);
+
+    expect(invalidParameterByName).toHaveProperty('name');
+    expect(invalidParameterByName.name).toHaveLength(2);
+
+    expect(invalidParameterByName.name[0]).toBe(invalidParameters[0]);
+    expect(invalidParameterByName.name[1]).toBe(invalidParameters[1]);
+
+    expect(invalidParameterByName).toHaveProperty('description');
+    expect(invalidParameterByName.description).toHaveLength(1);
+
+    expect(invalidParameterByName.description[0]).toBe(invalidParameters[2]);
+});
