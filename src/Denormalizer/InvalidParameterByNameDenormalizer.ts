1980de2c96ac16f4f5bd6a46e51caedc9fe1dc5b
Dominik Zogg <dominik.zogg@gmail.com>
2020-03-18 20:05:34 +0100

    simplify code

--- src/Denormalizer/InvalidParameterByNameDenormalizer.ts
+++ src/Denormalizer/InvalidParameterByNameDenormalizer.ts
@@ -1,10 +1,6 @@
 import InvalidParameter from '../Type/Error/InvalidParameter';
 
-const InvalidParameterByNameDenormalizer = (invalidParameters?: Array<InvalidParameter>) => {
-    if (!invalidParameters) {
-        return {};
-    }
-
+const InvalidParameterByNameDenormalizer = (invalidParameters: Array<InvalidParameter>) => {
     const errorsByFields: { [id: string]: Array<InvalidParameter>; } = {};
     invalidParameters.forEach((invalidParameter: InvalidParameter) => {
         if (!errorsByFields.hasOwnProperty(invalidParameter.name)) {
