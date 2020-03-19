485eb8335a1ca4739bee12a415c27298d4ec29a8
Dominik Zogg <dominik.zogg@gmail.com>
2020-03-19 20:37:56 +0100

    enhance create test

--- src/__tests__/Component/Page/Pet/Create.test.tsx
+++ src/__tests__/Component/Page/Pet/Create.test.tsx
@@ -1,33 +1,39 @@
 import React from 'react';
-import { render, waitForElement, act } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import Create from '../../../../Component/Page/Pet/Create';
-import HttpErrorPartial from '../../../../Component/Partial/HttpError';
-import PetForm from '../../../../Component/Form/PetForm';
-import Pet from '../../../../Type/Pet/Pet';
-import UnprocessableEntity from '../../../../Type/Error/UnprocessableEntity';
-import PetFormProps from '../../../../Type/Props/PetFormProps';
+import { createMemoryHistory } from 'history';
+import { fireEvent, render, waitForElement } from '@testing-library/react';
+import { Router } from 'react-router-dom';
 import * as ApiClientPet from '../../../../ApiClient/Pet';
+import Create from '../../../../Component/Page/Pet/Create';
+import HttpError from '../../../../Type/Error/HttpError';
+import Pet from '../../../../Type/Pet/Pet';
+import PetFormProps from '../../../../Type/Props/PetFormProps';
+import UnprocessableEntity from '../../../../Type/Error/UnprocessableEntity';
 
 jest.mock('../../../../ApiClient/Pet');
 
-let mockPetForm = (props: PetFormProps) => {};
-
 jest.mock('../../../../Component/Form/PetForm', () => {
-    return (props: PetFormProps) => {
-        return mockPetForm(props)
+    return ({ submitPet }: PetFormProps) => {
+        const submit = async () => {
+            await submitPet({ id: '', createdAt: '', name: '', vaccinations: [], _links: {} });
+        };
+
+        return (<button data-testid="test-button" onClick={submit}></button>);
+    };
+});
+
+jest.mock('../../../../Component/Partial/HttpError', () => {
+    return ({ httpError }: { httpError: HttpError; }) => {
+        return (<div className="row">httpError: {httpError.title}</div>);
     };
 });
 
 test('default', async () => {
-    mockPetForm = (props: PetFormProps) => {
-        return '___petForm___';
-    };
+    const history = createMemoryHistory();
 
     const { getByTestId } = render(
-        <MemoryRouter>
+        <Router history={history}>
             <Create />
-        </MemoryRouter>
+        </Router>
     );
 
     const navigationLeft = await waitForElement(() =>
@@ -35,49 +41,79 @@ test('default', async () => {
     );
 
     expect(navigationLeft.outerHTML).toBe(`
-    <main data-testid="page-pet-create" class="ui padded grid">
-        <div class="row"><h1 class="ui huge dividing header">Create Pet</h1></div>
-        <div class="row"><div class="ui attached segment">___petForm___</div></div>
-        <div class="row"><a class="ui button" role="button" href="/pet">List</a></div>
-    </main>
+        <main data-testid="page-pet-create" class="ui padded grid">
+            <div class="row"><h1 class="ui huge dividing header">Create Pet</h1></div>
+            <div class="row"><div class="ui attached segment"><button data-testid="test-button"></button></div></div>
+            <div class="row"><a class="ui button" role="button" href="/pet">List</a></div>
+        </main>
     `.replace(/\n {2,}/g, ''));
 });
 
-
-test('submit failure', async () => {
+test('unprocessable entity', async () => {
     ApiClientPet.CreatePet.mockResolvedValueOnce(new Promise((resolve) => {
-        resolve(new UnprocessableEntity({title: 'title'}));
+        resolve(new UnprocessableEntity({ title: 'title' }));
     }));
 
-    const pet: Pet = {
-        id: '22a18f88-56f2-41e9-82ab-27084961ba93',
-        createdAt: '2005-08-15T15:52:01+00:00',
-        name: 'Black Beauty',
-        vaccinations: [],
-        _links: {}
-    };
-
-    mockPetForm = ({ submitPet }: PetFormProps) => {
-        // submitPet(pet); after a click?
-
-        return '___petForm_submit_success___';
-    };
+    const history = createMemoryHistory();
 
     const { getByTestId } = render(
-        <MemoryRouter>
+        <Router history={history}>
             <Create />
-        </MemoryRouter>
+        </Router>
     );
+
+    const testButton = await waitForElement(() =>
+        getByTestId('test-button')
+    );
+
+    fireEvent.click(testButton);
 
     const navigationLeft = await waitForElement(() =>
         getByTestId('page-pet-create')
     );
 
     expect(navigationLeft.outerHTML).toBe(`
-    <main data-testid="page-pet-create" class="ui padded grid">
-        <div class="row"><h1 class="ui huge dividing header">Create Pet</h1></div>
-        <div class="row"><div class="ui attached segment">___petForm_submit_success___</div></div>
-        <div class="row"><a class="ui button" role="button" href="/pet">List</a></div>
-    </main>
+        <main data-testid="page-pet-create" class="ui padded grid">
+            <div class="row">httpError: title</div>
+            <div class="row"><h1 class="ui huge dividing header">Create Pet</h1></div>
+            <div class="row"><div class="ui attached segment"><button data-testid="test-button"></button></div></div>
+            <div class="row"><a class="ui button" role="button" href="/pet">List</a></div>
+        </main>
     `.replace(/\n {2,}/g, ''));
+});
+
+test('successful', async () => {
+    ApiClientPet.CreatePet.mockImplementationOnce((pet: Pet) => {
+        return new Promise((resolve) => resolve(pet));
+    });
+
+    const history = createMemoryHistory();
+
+    const { getByTestId } = render(
+        <Router history={history}>
+            <Create />
+        </Router>
+    );
+
+    const testButton = await waitForElement(() =>
+        getByTestId('test-button')
+    );
+
+    expect(history.location.pathname).toBe('/');
+
+    fireEvent.click(testButton);
+
+    const navigationLeft = await waitForElement(() =>
+        getByTestId('page-pet-create')
+    );
+
+    expect(navigationLeft.outerHTML).toBe(`
+        <main data-testid="page-pet-create" class="ui padded grid">
+            <div class="row"><h1 class="ui huge dividing header">Create Pet</h1></div>
+            <div class="row"><div class="ui attached segment"><button data-testid="test-button"></button></div></div>
+            <div class="row"><a class="ui button" role="button" href="/pet">List</a></div>
+        </main>
+    `.replace(/\n {2,}/g, ''));
+
+    expect(history.location.pathname).toBe('/pet');
 });
