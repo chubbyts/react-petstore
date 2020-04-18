9d056f562c0f2bf98585b2c8044dc2fd33a8e68a
Dominik Zogg <dominik.zogg@gmail.com>
2020-04-18 15:39:47 +0200

    purgecss

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,17 @@
 module.exports = {
     webpack: function (config, env) {
+        const purgecss = require('@fullhuman/postcss-purgecss')({
+
+            // Specify the paths to all of the template files in your project
+            content: [
+                './src/**/*.html',
+                './src/**/*.jsx',
+                './src/**/*.tsx',
+            ],
+
+            // Include any special characters you're using in this regular expression
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        });
 
         config.module.rules.push({
             test: /\.css$/,
@@ -12,6 +24,9 @@ module.exports = {
                             require('tailwindcss'),
                             require('postcss-nested'),
                             require('autoprefixer'),
+                            ...process.env.NODE_ENV === 'production'
+                                ? [purgecss]
+                                : []
                         ],
                     },
                 },
