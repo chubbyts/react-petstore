d3b2b76a469fa36763c3bc407b7e76caa94d61c0
Dominik Zogg <dominik.zogg@hotmail.com>
2022-07-17 23:17:26 +0200

    try to fix CI

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/__tests__/setup.ts",
+    coverage: {
+      reporter: ['text', 'html', 'lcov'],
+    },
   },
 });
